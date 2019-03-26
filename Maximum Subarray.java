8ec3d8be46e5d42bb9145f24acbed6ff70c0c03a
LeFTZz <48968790+LeFTZz@users.noreply.github.com>
2019-03-26 23:01:01 +0000

    #53
    Runtime: 6 ms
    Memory Usage: 38.5 MB

---
+++ Maximum Subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        // dp[i] is the maximum subarray that ended with nums[i]
+        dp[0] = nums[0];
+        int max_res = dp[0];
+        for(int i = 1; i < nums.length; i++){
+            dp[i] = Math.max(dp[i-1]+nums[i],nums[i]);
+            if(dp[i] > max_res){
+                max_res = dp[i];
+            }
+        }
+        return max_res;
+    }
+}
