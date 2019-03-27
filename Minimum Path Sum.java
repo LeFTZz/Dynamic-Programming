f8ef6a9c6c406af7fa0012b972ab10d1cfd453ee
LeFTZz <48968790+LeFTZz@users.noreply.github.com>
2019-03-27 00:23:06 +0000

    #64

---
+++ Minimum Path Sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if(grid.length == 0)
+            return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j <n; j++)
+                dp[i][j] = 0;
+        }
+        //边界值初始化
+        dp[0][0] = grid[0][0];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i ==0 && j == 0)
+                    continue;
+                if(i == 0 && j!=0)
+                    dp[i][j] = dp[i][j-1] + grid[i][j];
+                else if(j == 0 && i!=0)
+                    dp[i][j] = dp[i-1][j] + grid[i][j];
+                else{
+                    dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1]) + grid[i][j];
+                }
+            }
+        }
+        //System.out.println(grid[0].length);
+        return dp[m-1][n-1];
+    }
+}
